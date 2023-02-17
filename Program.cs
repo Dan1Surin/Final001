e2d450994fe7a19844efbfdefb3e7fdeebfb5bf1
Dan1Surin <dan1surin@gmail.com>
2023-02-17 14:39:25 +0300

    add program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+string[] Array = {"1234234","dsafd","=)","dfsfa","dd","324","3","d","asdfvc"};
+
+int size=0;
+int i=0;
+while (i<Array.Length)
+{
+    if (Array[i].Length<3)
+    {
+        size++;
+    }
+    i++;
+}
+
+string[] NewArray = new string[size];
+
+i=0;
+int a=0;
+while (i<Array.Length)
+{
+    if (Array[i].Length<3)
+    {
+        NewArray[a] = Array[i];
+        a++;
+    }
+    i++;
+}
+
+Console.WriteLine(string.Join(" , ",NewArray));
